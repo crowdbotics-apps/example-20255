9051038b1cd5c86ad8425a791b89fc752b6eed5e
Crowdbotics <team@crowdbotics.com>
2020-09-14 03:36:20 +0000

    Update components

--- src/features/BlankScreen0101069/screens/index.js
+++ src/features/BlankScreen0101069/screens/index.js
@@ -119,7 +119,165 @@ export class __Blank extends React.Component {
             borderRadius: 0,
             backgroundSize: "auto"
           }}
-        />
+        >
+          <View
+            style={{
+              width: "100%",
+              height: "100%",
+              marginLeft: 0,
+              marginRight: 0,
+              marginTop: 0,
+              marginBottom: 0,
+              paddingLeft: 0,
+              paddingRight: 0,
+              paddingTop: 0,
+              paddingBottom: 0,
+              overflow: "visible",
+              fontSize: 12,
+              color: "#000000",
+              backgroundColor: "#ffffff",
+              fontFamily: "Roboto-Regular",
+              flexDirection: "column",
+              flexWrap: "wrap",
+              justifyContent: "flex-start",
+              alignItems: "stretch",
+              alignContent: "stretch",
+              borderColor: "#000000",
+              borderStyle: "solid",
+              borderWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              borderRadius: 0,
+              backgroundSize: "auto"
+            }}
+          >
+            <Button
+              textStyle={{
+                fontSize: 12,
+                color: "#000000",
+                textAlign: "center",
+                fontFamily: "Roboto-Regular"
+              }}
+              style={{
+                width: "100%",
+                marginLeft: 0,
+                marginRight: 0,
+                marginTop: 5,
+                marginBottom: 5,
+                paddingLeft: 5,
+                paddingRight: 5,
+                paddingTop: 5,
+                paddingBottom: 5,
+                overflow: "visible",
+                fontSize: 12,
+                color: "#000000",
+                backgroundColor: "#3366FF",
+                fontFamily: "Roboto-Regular",
+                borderColor: "#000000",
+                borderStyle: "solid",
+                borderWidth: 0,
+                borderLeftWidth: 0,
+                borderRightWidth: 0,
+                borderTopWidth: 0,
+                borderBottomWidth: 0,
+                borderRadius: 0,
+                textAlign: "center",
+                textAlignVertical: "center",
+                textDecorationLine: "none",
+                textTransform: "none",
+                lineHeight: 12,
+                letterSpacing: 0
+              }}
+              onPress={() => alert("Pressed!")}
+            >
+              Press me!
+            </Button>
+            <Button
+              textStyle={{
+                fontSize: 12,
+                color: "#000000",
+                textAlign: "center",
+                fontFamily: "Roboto-Regular"
+              }}
+              style={{
+                width: "100%",
+                marginLeft: 0,
+                marginRight: 0,
+                marginTop: 5,
+                marginBottom: 5,
+                paddingLeft: 5,
+                paddingRight: 5,
+                paddingTop: 5,
+                paddingBottom: 5,
+                overflow: "visible",
+                fontSize: 12,
+                color: "#000000",
+                backgroundColor: "#3366FF",
+                fontFamily: "Roboto-Regular",
+                borderColor: "#000000",
+                borderStyle: "solid",
+                borderWidth: 0,
+                borderLeftWidth: 0,
+                borderRightWidth: 0,
+                borderTopWidth: 0,
+                borderBottomWidth: 0,
+                borderRadius: 0,
+                textAlign: "center",
+                textAlignVertical: "center",
+                textDecorationLine: "none",
+                textTransform: "none",
+                lineHeight: 12,
+                letterSpacing: 0
+              }}
+              onPress={() => alert("Pressed!")}
+            >
+              Press me!
+            </Button>
+            <Button
+              textStyle={{
+                fontSize: 12,
+                color: "#000000",
+                textAlign: "center",
+                fontFamily: "Roboto-Regular"
+              }}
+              style={{
+                width: "100%",
+                marginLeft: 0,
+                marginRight: 0,
+                marginTop: 5,
+                marginBottom: 5,
+                paddingLeft: 5,
+                paddingRight: 5,
+                paddingTop: 5,
+                paddingBottom: 5,
+                overflow: "visible",
+                fontSize: 12,
+                color: "#000000",
+                backgroundColor: "#3366FF",
+                fontFamily: "Roboto-Regular",
+                borderColor: "#000000",
+                borderStyle: "solid",
+                borderWidth: 0,
+                borderLeftWidth: 0,
+                borderRightWidth: 0,
+                borderTopWidth: 0,
+                borderBottomWidth: 0,
+                borderRadius: 0,
+                textAlign: "center",
+                textAlignVertical: "center",
+                textDecorationLine: "none",
+                textTransform: "none",
+                lineHeight: 12,
+                letterSpacing: 0
+              }}
+              onPress={() => alert("Pressed!")}
+            >
+              Press me!
+            </Button>
+          </View>
+        </View>
         <View
           style={{
             width: "50%",
